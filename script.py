e600e47f74565700e41a5b079adf2887e831d6f1
v_smiec <v_smiec@protonmail.com>
2022-01-23 15:12:58 +0100

    python script

---
+++ script.py
@@ -0,0 +1,14 @@
+import argparse
+
+if __name__ == "__main__":
+    # display help and version
+    parser.add_argument(
+        "--secrets". required=True
+    )
+    
+    # parsing arguments from user
+    args = parser.parse_args()
+    
+    secrets = args.secrets
+
+    print(secrets)
